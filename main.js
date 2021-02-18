193f5cf68d195ff70a577ab7a5413136a6078511
Ilya <ilyabolshakov44@gmail.com>
2021-02-18 22:14:28 +0300

    рисовалка на js

---
+++ main.js
@@ -0,0 +1,7 @@
+const fileSelector = document.getElementById('file-selector');
+  fileSelector.addEventListener('change', (event) => {
+    const fileList = event.target.files;
+    console.log(fileList);
+  });
+
+
